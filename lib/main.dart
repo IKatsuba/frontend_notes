d662ce401f4163e21304246223bafc54f57458b7
Igor Katsuba <ikatsuba@swiftcom.uk>
2018-12-06 16:54:54 +0300

    add cloud messaging

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,46 @@ import './enums/enums.dart';
 import './pages/pages.dart';
 import './services/services.dart';
 
+import 'dart:io';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
 void main() async {
   await config.init();
-  runApp(new MyApp());
+
+  runApp(new FrontendNotesApp());
 }
 
 FirebaseAnalytics analytics = FirebaseAnalytics();
 
-class MyApp extends StatelessWidget {
+class FrontendNotesApp extends StatefulWidget {
+  @override
+  _FrontendNotesAppState createState() => _FrontendNotesAppState();
+}
+
+class _FrontendNotesAppState extends State<FrontendNotesApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    _firebaseMessaging.getToken().then((token) {
+      print(token);
+    });
+
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print('on message $message');
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print('on resume $message');
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print('on launch $message');
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
